a4dda3897ae7baafdb1f46623e98f36fc2ec7b04
shelffim <shelffim0219@gmail.com>
2025-06-20 22:47:53 +0900

    day4

---
+++ day4/day4.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let surname:String="Lasso"
+var score:Double=0
+let username:String
+
+let playerName:String="Roy"
+var luckyNumber:Int=13
+let pi:Double=3.141
+var isAuthenticated:Bool=true
+
+var albums:[String]=["Red","Fearless"]
+username="@twostraws"
+
+var user:[String:String]=["id":"@asdaas"]
+var books:Set<String>=Set(["Harry Potter","To Kill a Mockingbird"])
+var soda:[String]=["Cake","Pepsi","Coca Cola"]
+
+var teams:[String]=[String]()
+var cities:[String]=[]
+var clues=[String]()
+
+enum UIStyle {
+    case light,dark,system
+}
+
+var style=UIStyle.light
+print(username)
