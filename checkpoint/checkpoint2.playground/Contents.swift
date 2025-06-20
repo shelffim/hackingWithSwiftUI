167339f565624005d47636206981178819631d68
shelffim <shelffim0219@gmail.com>
2025-06-20 22:57:05 +0900

    checkpoint2

---
+++ checkpoint/checkpoint2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let someArray=["Red","Fearless","Blue","Yellow","Red","Fearless","Blue","Yellow","Red","Yellow","Gray"]
+
+let setArray=Set(someArray)
+
+// 배열에 있는 항목의 개수
+print(someArray.count)
+// 배열에 있는 고유한 항목의 개수
+print(setArray.count)
