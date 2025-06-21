2f7f78ee2fa84d21e33cf6af494d669f043ceb54
shelffim <shelffim0219@gmail.com>
2025-06-21 15:22:52 +0900

    day5_1

---
+++ day5/day5.playground/Contents.swift
@@ -0,0 +1,102 @@
+import UIKit
+
+var score=85
+
+if score>80 {
+    print("Great job!")
+}
+
+let speed=88
+let percentage=85
+var age=18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+var numbers=[1,2,3]
+
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+
+print(numbers)
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymous" {
+    print("Welcome, \(name)")
+}
+
+var username="taylorswift13"
+
+if username == "" {
+    username = "Anonymous"
+}
+if username.count == 0 {
+    username = "Anonymous"
+}
+
+if username.isEmpty == true {
+    username = "Anonymous"
+}
+
+if username.isEmpty {
+    username = "Anonymous"
+}
+
+print("Welcome, \(username)!")
+
+let firstName = "Paul"
+let secondName = "Sophie"
+
+let firstAge = 40
+let secondAge = 10
+
+print(firstName == secondName)
+print(firstName != secondName)
+print(firstName < secondName)
+print(firstName >= secondName)
+
+print(firstAge == secondAge)
+print(firstAge != secondAge)
+print(firstAge < secondAge)
+print(firstAge >= secondAge)
+
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first < second)
+
