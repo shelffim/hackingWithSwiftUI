6d8aef87e7e4f2f8ee801c1ddbf57466df03df40
shelffim <shelffim0219@gmail.com>
2025-06-23 21:39:46 +0900

    day6_3

--- day6/MyPlayground.playground/Contents.swift
+++ day6/MyPlayground.playground/Contents.swift
@@ -1,23 +1,58 @@
 import UIKit
 
-var countdown = 10
+let filenames = ["me.jpg","work.txt","sophie.jpg","logo.psd"]
 
-while countdown > 0 {
-    print("\(countdown)...")
-    countdown -= 1
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    
+    print("Found picture: \(filename)")
 }
 
-print("Blast off!")
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
 
-let id = Int.random(in: 1...1000)
-
-let amount = Double.random(in: 0...1)
-
-var roll = 0
-
-while roll != 20 {
-    roll = Int.random(in: 1...20)
-    print("I rolled a \(roll)")
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
 }
 
-print("Critical hit!")
+print(multiples)
+
+let scores = [1,8,4,3,0,5,2]
+var count = 0
+
+for score in scores {
+    if score == 0 {
+        break
+    }
+    
+    count += 1
+}
+
+print("You had \(count) scores before you got 0.")
+
+let options = ["up","down","left","right"]
+
+let secretCombination = ["up","up","right"]
+
+outerLoop: for option1 in options {
+    for option2 in options {
+        for option3 in options {
+            print("In loop")
+            let attempt = [option1,option2,option3]
+            
+            if attempt == secretCombination {
+                print("The combination is \(attempt)!")
+                break outerLoop
+            }
+        }
+    }
+}
