9ce84aaf4245a8f4ee75fda39432f1a4e6e9f7e9
shelffim <shelffim0219@gmail.com>
2025-06-26 22:28:23 +0900

    day9_2

--- day9/day9.playground/Contents.swift
+++ day9/day9.playground/Contents.swift
@@ -1,75 +1,38 @@
 import UIKit
 
-func greetUser() {
-    print("Hi there!")
-}
-
-greetUser()
-
-var greetCopy : () -> Void = greetUser
-greetCopy()
-
-let sayHello = { (name: String) -> String in
-    "Hi \(name)!"
-}
-
-func getUserData(for id: Int) -> String {
-    if id == 1989 {
-        return "Taylor Swift"
-    } else {
-        return "Anonymous"
-    }
-}
-
-let data: (Int) -> String = getUserData
-let user = data(1989)
-print(user)
-
-sayHello("Taylor")
-
 let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
-let sortedTeam = team.sorted()
-print(sortedTeam)
 
-//func captainFirstSorted(name1: String, name2: String) -> Bool {
-//    if name1 == "Suzanne" {
-//        return true
-//    } else if name2 == "Suzanne" {
-//        return false
-//    }
-//    
-//    return name1 < name2
-//}
-//let captainFirstTeam = team.sorted(by: captainFirstSorted)
-let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
-    if name1 == "Suzanne" {
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
         return true
-    } else if name2 == "Suzanne" {
+    } else if $1 == "Suzanne" {
         return false
     }
-    return name1 < name2
-})
+    
+    return $0 <  $1
+}
 
 print(captainFirstTeam)
 
-func pay(user: String, amount: Int) {
-    // code
+let reverseTeam = team.sorted {
+    $0 > $1
 }
 
-let payment = { (user: String, amount: Int) in
-    // code
+let tOnly = team.filter { $0.hasPrefix("T")}
+print(tOnly)
+
+let uppercaseTeam =  team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+func animate(duration: Double, animations: () -> Void) {
+    print("Starting a \(duration) second animation...")
+    animations()
 }
 
-let payment2 = { (user: String) in
-    print("Paying \(user)...")
-}
+animate(duration: 3, animations: {
+    print("Fade out the image")
+})
 
-let payment3 = { (user: String) -> Bool in
-    print("Paying \(user)...")
-    return true
-}
-
-let payment4 = { () -> Bool in
-    print("Paying an anonymous person...")
-    return true
+animate(duration:3) {
+    print("Fade out the image")
 }
