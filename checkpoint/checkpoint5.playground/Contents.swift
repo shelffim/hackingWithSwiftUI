ba370067e16f4b919bd936d5bb139da4425a06e2
shelffim <shelffim0219@gmail.com>
2025-06-27 00:42:52 +0900

    checkpoint 5

--- checkpoint/checkpoint5.playground/Contents.swift
+++ checkpoint/checkpoint5.playground/Contents.swift
@@ -2,11 +2,10 @@ import UIKit
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
-func checkpoint(_ arr:[Int]) -> [String] {
-    let oddArr = arr.filter { $0.isMultiple(of: 2) == false }
-    let sortedOddArr = oddArr.sorted()
-    let mappingArr = sortedOddArr.map { String($0) + " is a lucky number"  }
-    return mappingArr
+let solve = { (_: [Int]) in
+    luckyNumbers.filter { !$0.isMultiple(of: 2)}
+        .sorted()
+        .map { print("\($0) is a lucky number.")}
 }
 
-print(checkpoint(luckyNumbers))
+solve(luckyNumbers)
