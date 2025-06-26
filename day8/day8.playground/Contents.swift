39c81ae7f9249725bcfe17e8b2bd53e5c9a28488
shelffim <shelffim0219@gmail.com>
2025-06-26 15:53:27 +0900

    day8_1

---
+++ day8/day8.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll(keepingCapacity: true)
+print(characters.count)
+characters.removeAll()
+print(characters.count)
+
+
+func findDirections(from: String, to: String, route: String = "fastest", avoidHighways: Bool = false) {
+    // code here
+}
+findDirections(from: "London", to: "Glasgow")
+findDirections(from: "London", to: "Glasgow", route: "scenic")
+findDirections(from: "London", to: "Glasgow", route: "scenic", avoidHighways: true)
