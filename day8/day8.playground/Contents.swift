7caf555730a91dd32d9da2b69c5290cb45753115
shelffim <shelffim0219@gmail.com>
2025-06-26 17:07:46 +0900

    day8_2

--- day8/day8.playground/Contents.swift
+++ day8/day8.playground/Contents.swift
@@ -1,25 +1,36 @@
 import UIKit
 
-func printTimesTables(for number: Int, end: Int = 12) {
-    for i in 1...end {
-        print("\(i) x \(number) is \(i * number)")
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
     }
 }
 
-printTimesTables(for: 5, end: 20)
-printTimesTables(for: 8)
+let string = "12345"
 
-var characters = ["Lana", "Pam", "Ray", "Sterling"]
-print(characters.count)
-characters.removeAll(keepingCapacity: true)
-print(characters.count)
-characters.removeAll()
-print(characters.count)
-
-
-func findDirections(from: String, to: String, route: String = "fastest", avoidHighways: Bool = false) {
-    // code here
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
 }
-findDirections(from: "London", to: "Glasgow")
-findDirections(from: "London", to: "Glasgow", route: "scenic")
-findDirections(from: "London", to: "Glasgow", route: "scenic", avoidHighways: true)
