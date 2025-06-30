1a970583ac265e66f064ea2a5506d879800e1e3d
shelffim <shelffim0219@gmail.com>
2025-06-30 21:46:43 +0900

    day10_1

---
+++ day10/day10.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining = 14
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on cacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
+var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
+
+let kane = Employee(name: "Lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+
+struct User {
+    var name: String
+    var age: Int
+    var city: String
+}
