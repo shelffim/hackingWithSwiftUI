4dfff21838ca07c1e755dd72d4235f21c27afa49
shelffim <shelffim0219@gmail.com>
2025-06-30 23:10:11 +0900

    day10_2

--- day10/day10.playground/Contents.swift
+++ day10/day10.playground/Contents.swift
@@ -1,51 +1,34 @@
 import UIKit
 
-struct Album {
-    let title: String
-    let artist: String
-    let year: Int
-    
-    func printSummary() {
-        print("\(title) (\(year)) by \(artist)")
-    }
-}
-
-let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
-let wings = Album(title: "Wings", artist: "BTS", year: 2016)
-
-print(red.title)
-print(wings.artist)
-
-red.printSummary()
-wings.printSummary()
+//struct Employee {
+//    let name: String
+//    var vacationRemaining: Int
+//}
+//
+//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//archer.vacationRemaining -= 5
+//print(archer.vacationRemaining)
+//archer.vacationRemaining -= 3
+//print(archer.vacationRemaining)
 
 struct Employee {
     let name: String
-    var vacationRemaining = 14
+    var vacationAllocated = 14
+    var vacationTaken = 0
     
-    mutating func takeVacation(days: Int) {
-        if vacationRemaining > days {
-            vacationRemaining -= days
-            print("I'm going on cacation!")
-            print("Days remaining: \(vacationRemaining)")
-        } else {
-            print("Oops! There aren't enough days remaining.")
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+        
+        set {
+            vacationAllocated = vacationTaken + newValue
         }
     }
 }
 
-var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
-archer.takeVacation(days: 5)
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
 print(archer.vacationRemaining)
-
-var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
-var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
-
-let kane = Employee(name: "Lana Kane")
-let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
-
-struct User {
-    var name: String
-    var age: Int
-    var city: String
-}
+archer.vacationRemaining = 5
+print(archer.vacationRemaining)
