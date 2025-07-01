6bb2c4b2db8a50e3d14a414f8dc0ed3b8146f497
shelffim <shelffim0219@gmail.com>
2025-07-01 13:44:26 +0900

    day10_3

--- day10/day10.playground/Contents.swift
+++ day10/day10.playground/Contents.swift
@@ -1,34 +1,34 @@
 import UIKit
 
-//struct Employee {
-//    let name: String
-//    var vacationRemaining: Int
-//}
-//
-//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
-//archer.vacationRemaining -= 5
-//print(archer.vacationRemaining)
-//archer.vacationRemaining -= 3
-//print(archer.vacationRemaining)
-
-struct Employee {
-    let name: String
-    var vacationAllocated = 14
-    var vacationTaken = 0
-    
-    var vacationRemaining: Int {
-        get {
-            vacationAllocated - vacationTaken
-        }
-        
-        set {
-            vacationAllocated = vacationTaken + newValue
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
         }
     }
 }
 
-var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
-archer.vacationTaken += 4
-print(archer.vacationRemaining)
-archer.vacationRemaining = 5
-print(archer.vacationRemaining)
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
