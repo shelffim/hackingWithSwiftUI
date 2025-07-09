c87bb261c25ed077ee3fc628e2737efbed210769
shelffim <shelffim0219@gmail.com>
2025-07-09 12:38:58 +0900

    day11_2

--- day11/day11.playground/Contents.swift
+++ day11/day11.playground/Contents.swift
@@ -1,28 +1,37 @@
 import UIKit
 
-struct BankAccount {
-    private var funds = 0
-    
-    mutating func deposit(amount: Int) {
-        funds += amount
-    }
-    
-    mutating func withdraw(amount: Int) -> Bool {
-        if funds >= amount {
-            funds -= amount
-            return true
-        } else {
-            return false
-        }
+struct School {
+    static var studentCount = 0
+
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
     }
 }
 
-var account = BankAccount()
-account.deposit(amount: 100)
-let success = account.withdraw(amount: 200)
+School.add(student: "Taylor Swift")
+print(School.studentCount)
 
-if success {
-    print("Withdrew money successfully")
-} else {
-    print("Failed to get the money")
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee {
+    let username: String
+    let password: String
+    
+    static let example = Employee(username: "cfdefqffq", password: "adqwdwdqd" )
+}
+
+struct Unwrap {
+    static let appURL = "https://itunes.apple.com/app/id1440611372"
+    
+    static var entropy = Int.random(in: 1...10000)
+    
+    static func getEntropy() -> Int {
+        entropy += 1
+        return entropy
+    }
 }
