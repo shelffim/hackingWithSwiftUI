b5c4771b1b794bc429d7d7f30c3a7a28c550e2b4
shelffim <shelffim0219@gmail.com>
2025-07-09 16:03:13 +0900

    day12_3

--- day12/day12.playground/Contents.swift
+++ day12/day12.playground/Contents.swift
@@ -1,38 +1,20 @@
 import UIKit
 
-class Employee {
-    let hours: Int
+class Vehicle {
+    let isEletric: Bool
     
-    init(hours: Int) {
-        self.hours = hours
-    }
-    
-    func printSummary() {
-        print("I work \(hours) hours a day.")
+    init(isElectric: Bool) {
+        self.isEletric = isElectric
     }
 }
 
-class Developer: Employee {
-    func work() {
-        print("I'm writing code for \(hours) hours.")
-    }
+class Car: Vehicle {
+    let isConvertible: Bool
     
-    override func printSummary() {
-        print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
     }
 }
 
-class Manager: Employee {
-    func work() {
-        print("I'm going to meetings for \(hours) hours.")
-    }
-}
-
-let robert = Developer(hours: 8)
-let joseph = Manager(hours: 10)
-robert.work()
-joseph.work()
-
-let novall = Developer(hours: 8)
-novall.printSummary()
-
+let teslaX = Car(isElectric: true, isConvertible: false)
