b97ee9178ff3ec8ac25fd666be333f014d7aadf5
shelffim <shelffim0219@gmail.com>
2025-07-09 13:27:34 +0900

    checkpoint 6 solve

--- checkpoint/checkpoint6.playground/Contents.swift
+++ checkpoint/checkpoint6.playground/Contents.swift
@@ -1,34 +1,33 @@
 import UIKit
 
-enum GearState: Int {
-    case gear1 = 1
-    case gear2 = 2
-    case gear3 = 3
-    case gear4 = 4
-    case gear5 = 5
-    case gear6 = 6
-    case gear7 = 7
-    case gear8 = 8
-    case gear9 = 9
-    case gear10 = 10
-    
-}
-
 struct Car {
-    let model: String = "BMW"
-    let sheetCount: Int = 4
+    let model: String
+    let seatCount: Int
     
-    private var currentGear: GearState = .gear1
+    private(set) var currentGear = 1
     
-    mutating func changeGear(_ manipulate: String) {
-        if manipulate == "up" {
-            if let changeGear = GearState(rawValue : currentGear.rawValue + 1) {
-                currentGear = changeGear
-            }
-        } else if manipulate == "down" {
-            if let changeGear = GearState(rawValue: currentGear.rawValue - 1) {
-                currentGear = changeGear
-            }
+    enum GearDirection {
+        case up, down, neutral
+    }
+    
+    mutating func changeGear(_ direction: GearDirection) {
+        switch direction {
+        case .down : currentGear -= 1
+            if currentGear < 1 { currentGear = 1 }
+        case .up : currentGear += 1
+            if currentGear > 10 { currentGear = 10 }
+        case .neutral :
+            currentGear = 1
         }
+        print("The \(model) is in gear \(currentGear)")
     }
 }
+
+var car = Car(model: "BMW", seatCount: 4)
+car.changeGear(.up)
+car.changeGear(.up)
+car.changeGear(.neutral)
+car.changeGear(.up)
+car.changeGear(.up)
+car.changeGear(.up)
+car.changeGear(.down)
