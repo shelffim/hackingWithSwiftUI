60fc2452b88c07a48ee595aa851bb4e157da94ca
shelffim <shelffim0219@gmail.com>
2025-07-10 16:07:46 +0900

    day13_3

--- day13/day13.playground/Contents.swift
+++ day13/day13.playground/Contents.swift
@@ -1,11 +1,48 @@
 import UIKit
 
-func getRandomNumber() -> some Equatable {
-    Int.random(in: 1...6)
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
 }
 
-func getRandomBool() -> some Equatable {
-    Bool.random()
+
+
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+quote.trim()
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+    
 }
 
-print(getRandomNumber() == getRandomBool())
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
