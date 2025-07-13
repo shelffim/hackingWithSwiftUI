342287afdd1c2c0a37708dac8f0b34fb3e694316
shelffim <shelffim0219@gmail.com>
2025-07-13 15:59:24 +0900

    day14_4

--- day14/day14.playground/Contents.swift
+++ day14/day14.playground/Contents.swift
@@ -1,31 +1,18 @@
 import UIKit
 
-let captains = [
-    "Enterprise": "Picard",
-    "Voyager": "Janeway",
-    "Defiant": "Sisko"
-]
+let names = ["Arya", "Bran", "Robb", "Sansa"]
 
-let new = captains["Serenity"] ?? "N/A"
-
-let new1 = captains["Serenity", default: "N/A"]
-
-let tvShows = ["Archer", "babylon 5", "Red Lasso"]
-let favorite = tvShows.randomElement() ?? "None"
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
 
 struct Book {
     let title: String
     let author: String?
 }
 
-let book = Book(title: "Beowulf", author: nil)
-let author = book.author ?? "Unknown"
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
 print(author)
 
-let input = ""
-let number = Int(input) ?? 0
-print(number)
-
-let scores = ["Picard": 800, "Data": 7000, "Troi": 900]
-//let crusherScore = scores["Crusher"] ?? 0
-let crusherScore = scores["Crusher", default: 0]
+let arts = ["Vincent": "van Gogh", "Pablo": "Picasso", "Claude": "Monet"]
+let surnameLetter = arts["Vincent"]?.first?.uppercased() ?? "?"
