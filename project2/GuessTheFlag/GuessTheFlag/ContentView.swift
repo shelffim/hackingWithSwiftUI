6200faf0599cb8d72c0720a910023c1cb9f8d2ea
shelffim <shelffim0219@gmail.com>
2025-07-20 18:15:00 +0900

    day20_5

--- project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,20 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        Button {
-            print("Edit button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK", role: .cancel) {}
+        } message: {
+            Text("Please read this.")
         }
     }
     
-    func executeDelete() {
-        print("Now deleting...")
-    }
 }
 
 #Preview {
