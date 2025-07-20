09d77693852c08ef790747c6e47654ab5290641c
shelffim <shelffim0219@gmail.com>
2025-07-20 16:47:58 +0900

    day20_1

---
+++ project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by 박현준 on 7/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack(spacing: 20) {
+                Text("1")
+                Text("1")
+                Text("1")
+            }
+            HStack(spacing: 20) {
+                Text("1")
+                Text("1")
+                Text("1")
+            }
+            HStack(spacing: 20) {
+                Text("1")
+                Text("1")
+                Text("1")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 박현준 on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
