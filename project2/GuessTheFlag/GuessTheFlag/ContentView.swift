933104431960f1fd9a34740cc523d53833bcaafc
shelffim <shelffim0219@gmail.com>
2025-07-20 17:14:19 +0900

    day20_3

--- project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,19 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
-        
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.red.gradient)
     }
 }
 
