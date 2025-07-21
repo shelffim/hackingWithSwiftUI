173bff46d867a9201060818e1e4669b073406b0e
shelffim <shelffim0219@gmail.com>
2025-07-21 17:15:34 +0900

    day21_1

--- project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("OK", role: .cancel) {}
-        } message: {
-            Text("Please read this.")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
     }
     
