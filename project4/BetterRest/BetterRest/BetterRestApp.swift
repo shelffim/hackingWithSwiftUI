5311e4bada92fcf30907af496abf099892e604bf
shelffim <shelffim0219@gmail.com>
2025-07-30 13:24:35 +0900

    day26-1

---
+++ project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 박현준 on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project4/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by 박현준 on 7/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+}
+
+#Preview {
+    ContentView()
+}
