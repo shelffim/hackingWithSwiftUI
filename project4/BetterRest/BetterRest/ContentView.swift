136468dc0a3fe5f51db62373525153697ed47843
shelffim <shelffim0219@gmail.com>
2025-07-30 14:23:40 +0900

    day26-4

--- project4/BetterRest/BetterRest/ContentView.swift
+++ project4/BetterRest/BetterRest/ContentView.swift
@@ -8,22 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
-        Text(Date.now, format: .dateTime.hour().minute())
-        Text(Date.now, format: .dateTime.day().month().year())
-        Text(Date.now.formatted(date: .long, time: .shortened))
-    }
-    
-    func exampleDates() {
-//        let components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-        
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 
