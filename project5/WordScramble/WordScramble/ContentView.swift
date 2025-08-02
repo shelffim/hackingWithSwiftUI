e9d1698b36f526b7809b460f110e77a45716f504
shelffim <shelffim0219@gmail.com>
2025-08-02 16:31:55 +0900

    day29-3

--- project5/WordScramble/WordScramble/ContentView.swift
+++ project5/WordScramble/WordScramble/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -18,12 +19,24 @@ struct ContentView: View {
         .padding()
     }
     
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
-            }
-        }
+    func testStrings() {
+        let input = """
+            a
+            b
+            c
+            """
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
