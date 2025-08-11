350ef0011c5a31803cbe30a8262b18cde4821205
shelffim <shelffim0219@gmail.com>
2025-08-11 14:18:37 +0900

    day32-1

---
+++ project6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 박현준 on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
