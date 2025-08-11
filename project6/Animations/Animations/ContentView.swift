65a265c9562dc06ffade0415d93fe6d1b07e458c
shelffim <shelffim0219@gmail.com>
2025-08-11 15:20:22 +0900

    day32-2

--- project6/Animations/Animations/ContentView.swift
+++ project6/Animations/Animations/ContentView.swift
@@ -11,15 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+            // animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
