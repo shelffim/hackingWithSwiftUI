37b2ddf964decd4587cb158f75f45b9468106234
shelffim <shelffim0219@gmail.com>
2025-08-13 14:47:04 +0900

    day33-1

--- project6/Animations/Animations/ContentView.swift
+++ project6/Animations/Animations/ContentView.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
+    
     var body: some View {
         Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
+            enabled.toggle()
         }
-        .padding(50)
-        .background(.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled)
         .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
     }
 }
 
